672f0d37c7b0e0f44031ead9d6caf02f8d182e23
Halit Sever <halitsever@protonmail.com>
2023-09-05 00:37:47 +0300

    fix: 🐛 remove ts-node-dev from back-end project

--- back-end/src/utils/console.utils.ts
+++ back-end/src/utils/console.utils.ts
@@ -2,15 +2,15 @@ import chalk from "chalk";
 import appConfig from "../config/app.config";
 
 const infoMessage = (message: string) => {
-  console.log(chalk.bgBlue(message));
+  console.log(chalk.bgGreen(message));
 };
 
 const errorMessage = (message: string) => {
-  return appConfig.SHOW_ERRORS ? console.log(chalk.bgRed(message)) : null;
+  return appConfig.SHOW_ERRORS ? console.log(chalk.bgRed.white(message)) : null;
 };
 
 const debugMessage = (message: string) => {
-  return appConfig.DEBUG_MODE ? console.log(chalk.bgGray(message)) : null;
+  return appConfig.DEBUG_MODE ? console.log(chalk.bgGray.white(message)) : null;
 };
 
 export { infoMessage, errorMessage, debugMessage };
