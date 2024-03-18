d5fb6f44de5ef8258d7edfaee0f8b5d97b85a9a4
Halit Sever <halitsever@protonmail.com>
2024-03-19 01:21:00 +0300

    refactor: 💡 update folder names, add build.js script (empty)
    back-end => backend, front-end => frontend
    
    BREAKING CHANGE: 🧨 Build scripts not working, docker not working

--- backend/test/app.e2e-spec.ts
+++ backend/test/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import { AppModule } from './../src/app.module';
+import { AppModule } from '../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
